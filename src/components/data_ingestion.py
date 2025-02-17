ee999e3e3cf78300409a630f6879c32a00db5716
Mohan <krishna@savatas-MacBook-Air.local>
2025-02-17 18:07:37 -0500

    Hyper parameter training

--- src/components/data_ingestion.py
+++ src/components/data_ingestion.py
@@ -9,6 +9,7 @@ from dataclasses import dataclass
 
 from src.components.data_transformation import DataTransformation
 from src.components.data_transformation import DataTransformationConfig
+from src.components.model_trainer import ModelTrainer
 
 @dataclass
 class DataIngestionConfig:
@@ -37,7 +38,7 @@ class DataIngestion:
 
             train_set.to_csv(self.data_ingestion_config.train_data_path, index=False, header=True)
 
-            train_set.to_csv(self.data_ingestion_config.test_data_path, index=False, header=True)
+            test_set.to_csv(self.data_ingestion_config.test_data_path, index=False, header=True)
 
             logging.info("ingestion is completed")
             return (
@@ -51,4 +52,7 @@ if __name__ == "__main__":
     obj = DataIngestion()
     train_data, test_data = obj.initiate_data_ingestion()
     data_transformation = DataTransformation()
-    data_transformation.initiate_data_transformation(train_data,test_data)
+    train_arr, test_arr, _= data_transformation.initiate_data_transformation(train_data,test_data)
+
+    modeltrainer = ModelTrainer()
+    print(modeltrainer.initiate_model_trainer(train_arr, test_arr))
--- src/utils.py
+++ src/utils.py
@@ -3,6 +3,8 @@ import sys
 import numpy as np
 import pandas as pd
 import dill
+from sklearn.metrics import r2_score
+from sklearn.model_selection import GridSearchCV
 
 from src.exception import CustomException
 
@@ -13,5 +15,28 @@ def save_object(file_path, obj):
 
         with open(file_path, 'wb') as f:
             dill.dump(obj, f)
+    except Exception as e:
+        raise CustomException(e, sys)
+
+def evaluate_models(X_train,y_train,X_test,y_test,models,param):
+    try:
+       
+       report = {}
+       for i in range(len(list(models))):
+            model = list(models.values())[i]
+            parameters = param[list(models.keys())[i]]
+
+            gridsearch = GridSearchCV(model,parameters, cv=3)
+            gridsearch.fit(X_train,y_train)
+
+            model.set_params(**gridsearch.best_params_)
+
+            model.fit(X_train, y_train)
+            y_train_pred = model.predict(X_train)
+            y_test_pred = model.predict(X_test)
+            train_model_score = r2_score(y_train, y_train_pred)
+            test_model_score = r2_score(y_test, y_test_pred)
+            report[list(models.keys())[i]] = test_model_score
+       return report
     except Exception as e:
         raise CustomException(e, sys)
